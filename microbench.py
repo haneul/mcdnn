7714f04c1ba2df6965a7f3e7c0810bbdb89e3db4
Seungyeop Han <syhan@cs.washington.edu>
2015-03-02 17:00:37 -0800

    edit mbench

--- microbench.py
+++ microbench.py
@@ -8,18 +8,16 @@ def procStatus():
             return int(line.split(":",1)[1].strip().split(' ')[0])
     return None
 r1 = resource.getrusage(resource.RUSAGE_SELF)
-net = caffe.Net(sys.argv[1]+".Model.prototxt", sys.argv[1]+".Model.caffemodel")
-net.set_phase_test()
-net.set_mode_gpu()
+net = caffe.Net(sys.argv[1]+".prototxt", sys.argv[1]+".caffemodel")
+caffe.set_phase_test()
+caffe.set_mode_gpu()
 del net
 m1 = procStatus()
 r2 = resource.getrusage(resource.RUSAGE_SELF)
 #print (r2.ru_maxrss - r1.ru_maxrss) / 1024.0
 beg = time.time()
-net2 = caffe.Net(sys.argv[1]+".Model.prototxt", sys.argv[1]+".Model.caffemodel")
+net2 = caffe.Net(sys.argv[1]+".prototxt", sys.argv[1]+".caffemodel")
 end = time.time()
-net2.set_phase_test()
-net2.set_mode_gpu()
 r3 = resource.getrusage(resource.RUSAGE_SELF)
 m2 = procStatus()
 print "loading time (ms): ",
@@ -35,6 +33,6 @@ for i in range(10):
 beg = time.time()
 prediction = net2.forward_all(data=data)
 end = time.time()
-print "compute time (s): ",
+print "compute time (ms): ",
 print (end-beg) * 1000
 
