b11eafd823c3edda01109ed06d5e16028f0d7aff
Seungyeop Han <syhan@cs.washington.edu>
2015-03-25 20:36:01 -0700

    model pb2

--- model_pb2.py
+++ model_pb2.py
@@ -19,7 +19,7 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='model.proto',
   package='uw.syhan.mcdnn',
-  serialized_pb=_b('\n\x0bmodel.proto\x12\x0euw.syhan.mcdnn\"\x94\x02\n\x0eModelParameter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12)\n\x04type\x18\x02 \x02(\x0e\x32\x1b.uw.syhan.mcdnn.RequestType\x12\x0f\n\x07\x63ompute\x18\x03 \x01(\t\x12\x0e\n\x06memory\x18\x04 \x01(\t\x12\x17\n\x0floading_latency\x18\x05 \x01(\x02\x12\x10\n\x08\x61\x63\x63uracy\x18\x06 \x01(\x02\x12\x17\n\x0f\x63ompute_latency\x18\x07 \x01(\x02\x12\x16\n\x0e\x63ompute_energy\x18\x08 \x01(\x02\x12\x19\n\x11s_compute_latency\x18\t \x01(\x02\x12\x19\n\x11s_loading_latency\x18\n \x01(\x02\x12\x16\n\x0eloading_energy\x18\x0b \x01(\x02\"P\n\x10\x41pplicationModel\x12\x0c\n\x04name\x18\x01 \x01(\t\x12.\n\x06models\x18\x02 \x03(\x0b\x32\x1e.uw.syhan.mcdnn.ModelParameter*#\n\x0bRequestType\x12\x08\n\x04\x46\x41\x43\x45\x10\x01\x12\n\n\x06OBJECT\x10\x02')
+  serialized_pb=_b('\n\x0bmodel.proto\x12\x0euw.syhan.mcdnn\"\xbe\x03\n\x0eModelParameter\x12\x0c\n\x04name\x18\x01 \x01(\t\x12)\n\x04type\x18\x02 \x02(\x0e\x32\x1b.uw.syhan.mcdnn.RequestType\x12\x0f\n\x07\x63ompute\x18\x03 \x01(\t\x12\x0e\n\x06memory\x18\x04 \x01(\t\x12\x17\n\x0floading_latency\x18\x05 \x01(\x02\x12\x10\n\x08\x61\x63\x63uracy\x18\x06 \x01(\x02\x12\x17\n\x0f\x63ompute_latency\x18\x07 \x01(\x02\x12\x16\n\x0e\x63ompute_energy\x18\x08 \x01(\x02\x12\x19\n\x11s_compute_latency\x18\t \x01(\x02\x12\x19\n\x11s_loading_latency\x18\n \x01(\x02\x12\x16\n\x0eloading_energy\x18\x0b \x01(\x02\x12\x19\n\x11sp_loading_energy\x18\x0c \x01(\x02\x12\x19\n\x11sp_compute_energy\x18\r \x01(\x02\x12\x1a\n\x12sp_loading_latency\x18\x0e \x01(\x02\x12\x1a\n\x12sp_compute_latency\x18\x0f \x01(\x02\x12\x1c\n\x14sp_s_loading_latency\x18\x10 \x01(\x02\x12\x1c\n\x14sp_s_compute_latency\x18\x11 \x01(\x02\"P\n\x10\x41pplicationModel\x12\x0c\n\x04name\x18\x01 \x01(\t\x12.\n\x06models\x18\x02 \x03(\x0b\x32\x1e.uw.syhan.mcdnn.ModelParameter*#\n\x0bRequestType\x12\x08\n\x04\x46\x41\x43\x45\x10\x01\x12\n\n\x06OBJECT\x10\x02')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -40,8 +40,8 @@ _REQUESTTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   options=None,
-  serialized_start=392,
-  serialized_end=427,
+  serialized_start=562,
+  serialized_end=597,
 )
 _sym_db.RegisterEnumDescriptor(_REQUESTTYPE)
 
@@ -135,6 +135,48 @@ _MODELPARAMETER = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    _descriptor.FieldDescriptor(
+      name='sp_loading_energy', full_name='uw.syhan.mcdnn.ModelParameter.sp_loading_energy', index=11,
+      number=12, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='sp_compute_energy', full_name='uw.syhan.mcdnn.ModelParameter.sp_compute_energy', index=12,
+      number=13, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='sp_loading_latency', full_name='uw.syhan.mcdnn.ModelParameter.sp_loading_latency', index=13,
+      number=14, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='sp_compute_latency', full_name='uw.syhan.mcdnn.ModelParameter.sp_compute_latency', index=14,
+      number=15, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='sp_s_loading_latency', full_name='uw.syhan.mcdnn.ModelParameter.sp_s_loading_latency', index=15,
+      number=16, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='sp_s_compute_latency', full_name='uw.syhan.mcdnn.ModelParameter.sp_s_compute_latency', index=16,
+      number=17, type=2, cpp_type=6, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -147,7 +189,7 @@ _MODELPARAMETER = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=32,
-  serialized_end=308,
+  serialized_end=478,
 )
 
 
@@ -183,8 +225,8 @@ _APPLICATIONMODEL = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=310,
-  serialized_end=390,
+  serialized_start=480,
+  serialized_end=560,
 )
 
 _MODELPARAMETER.fields_by_name['type'].enum_type = _REQUESTTYPE
