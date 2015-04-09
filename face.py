dca034d499aa3f6bfb1796681fb3029716516525
Seungyeop Han <syhan@cs.washington.edu>
2015-04-08 17:10:34 -0700

    face

--- face.py
+++ face.py
@@ -3,7 +3,7 @@ import os, sys
 
 # model dir
 #model_path = "/home/haichen/models/caffe/"
-model_path = "face_models/"
+model_path = "/media/sdcard/"
 data_path = "/home/haichen/datasets/MSRBingFaces/"
 from example import * 
 #lbl = face_recognition(os.path.join(data_path, "iu/112.jpg"), [152,152], [152,152], os.path.join(model_path, "D0.prototxt"), os.path.join(model_path, "D0.caffemodel"))
