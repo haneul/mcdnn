7ae611ca5b7ad905b058410adf1d3f07e85dba93
Seungyeop Han <syhan@cs.washington.edu>
2015-06-10 14:31:38 -0700

    now recognizing

--- glimpse.py
+++ glimpse.py
@@ -67,18 +67,17 @@ def load_colormap():
             lst.append((sp[3], sp[2], sp[1]))
     return lst 
 
-class Thermal:
-    def __init__(self, handle):
+from threading import Thread
+class Thermal(Thread):
+    def __init__(self, handle, ctrl):
+        Thread.__init__(self)
         self.handle = handle
         self.sensors = {}
         self.Image = np.zeros((TotalColumns, TotalRows))#[[0]*TotalRows] * TotalColumns
         self.width = 640
         self.height = 480
         self.Im = create_blank(self.width, self.height)
-        self.pixel_width = self.width / TotalColumns
-        self.pixel_height = self.height / TotalRows
-        self.color_map = load_colormap()
-        self.len_cm = len(self.color_map)
+        self.ctrl = ctrl
 
     def _read_message(self):
         message = ''
@@ -100,6 +99,7 @@ class Thermal:
 
         scale = 1.0
         offset = 0.0
+        self.Image = np.zeros((TotalColumns, TotalRows))#[[0]*TotalRows] * TotalColumns
 
         for sensor_id in range(SENSORS_TOTAL):
             i = 0
@@ -113,14 +113,20 @@ class Thermal:
                     r += 1
                     i += 1
                 c += 1
-        self.show_image()
+        self.ctrl.lock.acquire()
+        self.ctrl.tempQueue.append(self.Image)
+        self.ctrl.haveTemp = True
+        self.ctrl.lock.release()
+        self.ctrl.event()
+        return True
+        #return self.show_image()
 
     def show_image(self):
         for r in range(TotalRows):
             for c in range(TotalColumns):
                 temp = self.Image[c][r]
-                #print(temp)
                 if temp < 20: temp = 20
+                if temp > 40: temp = 39.9
                 color_i = int((temp - 20) / 20.0 * self.len_cm)
                 color = self.color_map[color_i]
                 #print(self.pixel_width)
@@ -128,20 +134,140 @@ class Thermal:
                 y = r * self.pixel_height
                 cv2.rectangle(self.Im, (x, y), (x+self.pixel_width, y+self.pixel_height), color, -1)
         cv2.imshow('frame', self.Im)
-        cv2.waitKey(1)
-        
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            return False
+        return True
+       
+    def run(self):
+        while True:
+            try:
+                if not self._run_():
+                    break
+            except Exception as e:
+                print("error", e)
+                break
+         
+
+    def _run_(self):
+        message = self._read_message()
+        if len(message) == 0: return True
+        header = MSG_HEADER(message[:4])
+        msg = MsgFactory.create(header.MessageID, message)
+        if msg != None:
+            return msg.handle(self)
+        else:
+            #print(header.MessageID, header.MessageSize)
+            return True
+
+import Queue
+from threading import Lock
+from util import sendFrame
+import request_pb2
+HOST = "archon.cs.washington.edu"
+PORT = 9999
+label = ''
+
+class SendThread(Thread):
+    def __init__(self, frame):
+        Thread.__init__(self)
+        self.frame = frame
 
     def run(self):
-        for i in range(600):    
-            message = self._read_message()
-            if len(message) == 0: continue
-            header = MSG_HEADER(message[:4])
-            msg = MsgFactory.create(header.MessageID, message)
-            if msg != None:
-                msg.handle(self)
-            else:
-                print(header.MessageID, header.MessageSize)
+        global label
+        try:
+            label_, latency = sendFrame(self.frame, HOST, PORT, request_pb2.OBJECT)
+        except:
+            return
+        label = label_
+        print(label)
+        
+import time
+last = time.time()
 
+class ShowThread(Thread):
+    def __init__(self, im, temp):
+        Thread.__init__(self)
+        self.Image = im
+        self.temp = temp
+        self.width = 640
+        self.height = 480
+        self.temp_im = create_blank(self.width, self.height)
+        self.pixel_width = self.width / TotalColumns
+        self.pixel_height = self.height / TotalRows
+        self.color_map = load_colormap()
+        self.len_cm = len(self.color_map)
+
+    def show_image(self):
+        for r in range(TotalRows):
+            for c in range(TotalColumns):
+                temp = self.temp[c][r]
+                if temp < 20: temp = 20
+                if temp > 40: temp = 39.9
+                color_i = int((temp - 20) / 20.0 * self.len_cm)
+                color = self.color_map[color_i]
+                #print(self.pixel_width)
+                x = c * self.pixel_width
+                y = r * self.pixel_height
+                cv2.rectangle(self.temp_im, (x, y), (x+self.pixel_width, y+self.pixel_height), color, -1)
+        cv2.imshow('frame', self.temp_im)
+        return True
+
+    def run(self):
+        #self.show_image()
+        temp = self.temp > 32
+        shape = temp.shape
+        rightmost = -1
+        topmost = 100
+        found = False
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                if temp[i][j]:
+                    if i > rightmost: rightmost = i
+                    if j < topmost: topmost = j
+                    found = True
+        if found: 
+            global last
+            h = 300
+            x = max(0, min(int(rightmost/32.0*640 - h/2), 640-h))
+            y = max(0, min(int(topmost/24.0*480)-30, 480-h))
+            cv2.rectangle(self.Image, (x, y), (x+h, y+h), (255,0,0), 3)
+            now = time.time()
+            if now-last > 1:
+                SendThread(self.Image[y:y+h, x:x+h]).start()
+                last = now
+        
+        cv2.imshow('rgb', self.Image)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            return False
+
+
+class GlimpseController:
+    def __init__(self):
+        self._reset()
+        self.lock = Lock()
+
+    def _reset(self):
+        self.imageQueue = []
+        self.tempQueue = [] 
+        self.haveImage = False
+        self.haveTemp = False
+
+    def event(self):
+        self.lock.acquire()
+        if self.haveImage and self.haveTemp:
+            im = self.imageQueue[-1]
+            temp = self.tempQueue[-1]
+            self._reset()
+            self.lock.release() 
+            st = ShowThread(im, temp)
+            st.start()
+        else: 
+            self.lock.release() 
+
+
+ctrl = GlimpseController()
 
 data = 0x00
 handle.claimInterface(0)
@@ -150,9 +276,16 @@ try:
 except:
     pass
 
-thermal = Thermal(handle)
-thermal.run()
+thermal = Thermal(handle, ctrl)
+thermal.start()
+
+from pg_rgb import ImageThread
+im = ImageThread(ctrl)
+im.start()
+
+thermal.join()
+im.join()
 handle.releaseInterface(0)
 handle.close()
-cv2.waitKey()
+#cv2.waitKey()
 cv2.destroyAllWindows()
--- pg_rgb.py
+++ pg_rgb.py
@@ -2,24 +2,51 @@ import flycapture2 as fc2
 import numpy as np
 import cv2
 
-def run():
-    print fc2.get_library_version()
-    c = fc2.Context()
-    if (c.get_num_of_cameras() < 1): return
-    c.connect(*c.get_camera_from_index(0))
-    m, f = c.get_video_mode_and_frame_rate()
-    #p = c.get_property(fc2.FRAME_RATE)
-    c.start_capture()
-    im = fc2.Image()
-    while True:
-        c.retrieve_buffer(im)
-        a = np.array(im)
-        cv2.imshow('rgb', a)
-        key = cv2.waitKey(1)
-        if key & 0xFF == ord('q'):
-            break
-    c.stop_capture()
-    c.disconnect()
+
+from threading import Thread
+
+class ImageThread(Thread):
+    def __init__(self, ctrl = None):
+        Thread.__init__(self)
+        self.ctrl = ctrl 
+
+    def run(self):
+        self._run()
+
+    def _run(self, keep = True):
+        print fc2.get_library_version()
+        c = fc2.Context()
+        if (c.get_num_of_cameras() < 1): return
+        c.connect(*c.get_camera_from_index(0))
+        m, f = c.get_video_mode_and_frame_rate()
+        print(c.get_format7_configuration())
+        c.start_capture()
+        im = fc2.Image()
+        while True:
+            c.retrieve_buffer(im)
+            a = np.array(im)
+            a = cv2.resize(a, (640,480))
+            a = cv2.flip(a, 0)
+            if self.ctrl:
+                self.ctrl.lock.acquire()
+                self.ctrl.imageQueue.append(a)
+                self.ctrl.haveImage = True
+                self.ctrl.lock.release()
+                self.ctrl.event() 
+                #cv2.imshow('rgb1', a)
+                #cv2.waitKey(1)
+            else:
+                cv2.imshow('rgb', a)
+
+                if not keep:
+                    key = cv2.waitKey()
+                    break
+                else:
+                    key = cv2.waitKey(1)
+                    if key & 0xFF == ord('q'): break
+        c.stop_capture()
+        c.disconnect()
 
 if __name__ == "__main__":
-    run()
+    im = ImageThread()
+    im._run(False)
